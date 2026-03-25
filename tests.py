f16068097c5f6ec42a78757812e307e3cf08a47e
vitormoreira2r <vitormoreira2r@gmail.com>
2026-03-25 20:56:18 -0300

    Commit 2: Creating 10 unit tests.

--- tests.py
+++ tests.py
@@ -34,3 +34,96 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+
+def test_crete_choice_with_invalid_text():
+    question = Question(title='q1')
+    with pytest.raises(Exception):
+        question.add_choice(12, True)
+
+def test_remove_choice_that_not_exist():
+    question = Question(title='q1')
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(2)
+
+def test_find_choice_in_question_without_choices():
+    question = Question(title='q1')
+    with pytest.raises(Exception):
+        question.set_correct_choices([1,2])
+
+def test_find_choices_in_question():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1,2])
+
+def test_return_none_correct_choice():
+    question = Question(title='q1', points=2, max_selections=2)
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1,2])
+
+    choices = question.correct_selected_choices([3, 4])
+
+    assert len(choices) == 0
+
+def test_return_correct_choices():
+    question = Question(title='q1', points=2, max_selections=2)
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1,2])
+
+    choices = question.correct_selected_choices([1, 2])
+
+    assert len(choices) == 2
+
+def test_pass_max_selections():
+    question = Question(title='q1', points=2, max_selections=1)
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1,2])
+
+    with pytest.raises(Exception):
+        choices = question.correct_selected_choices([1, 2])
+
+def test_find_choice_after_remove_all_choices():
+    question = Question(title='q1', points=2, max_selections=1)
+
+    question.add_choice('a', False)
+
+    question.remove_all_choices()
+
+    with pytest.raises(Exception):
+        choices = question._find_choice_by_id(1)
+
+def test_remove_choice_y_id():
+    question = Question(title='q1', points=2, max_selections=1)
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+
+    question.remove_choice_by_id(1)
+
+    len(question.choices) == 1
+
+def test_remove_choice_using_invalid_id():
+    question = Question(title='q1', points=2, max_selections=1)
+
+    question.add_choice('a', False)
+
+    question.add_choice('b', False)
+    with pytest.raises(Exception):
+        question.remove_choice_by_id('adawdawdaw')
